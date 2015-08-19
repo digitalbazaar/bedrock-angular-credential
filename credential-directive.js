e187fcd2a2ce24cddf0b25c4ed1c6b72f937d4c4
Dave Longley <dlongley@digitalbazaar.com>
2015-08-19 16:25:29 -0400

    Use credential library to provide groups.

--- credential-directive.js
+++ credential-directive.js
@@ -6,17 +6,18 @@
  * @author David I. Lehn
  * @author Dave Longley
  */
-define(['jsonld'], function(jsonld) {
+define(['jsonld', 'underscore'], function(jsonld, _) {
 
 'use strict';
 
 /* @ngInject */
-function factory(brAlertService, config) {
+function factory(brAlertService, brCredentialLibraryService, config) {
   return {
     restrict: 'E',
     scope: {
       credential: '=brCredential',
       groups: '=?brGroups',
+      library: '=?brLibrary',
       showActions: '=?brShowActions'
     },
     templateUrl: requirejs.toUrl('bedrock-angular-credential/credential.html'),
@@ -41,23 +42,55 @@ function factory(brAlertService, config) {
       }
       // TODO: use bindToController in directive definition above instead
       model.credential = credential;
-      model.groups = credential.sysLayout || scope.groups;
+
+      // FIXME: running async code here can cause race conditions such that
+      // the model isn't in sync with itself
+      model.groups = [];
+      model.actionables = [];
+      Promise.all([_updateGroups(), _updateActionables()]).then(function() {
+        scope.$apply();
+      });
+    }, true);
+
+    function _updateGroups() {
+      var groups = model.credential.sysLayout || scope.groups;
+      var promise;
+      if(scope.library || !groups) {
+        promise = brCredentialLibraryService.getLibrary(scope.library)
+          .then(function(library) {
+            // pick out groups that match credential types
+            var types = _.flatten(jsonld.getValues(model.credential, 'type'));
+            return _.values(_.pick(library.groups, types));
+          }).catch(function(err) {
+            brAlertService.add('error', err, {scope: scope});
+          });
+      } else {
+        promise = Promise.resolve(model.credential.sysLayout || scope.groups);
+      }
+      return promise.then(function(groups) {
+        model.groups = groups;
+      });
+    }
+
+    function _updateActionables() {
+      var promise;
 
       // filter out any objects with potential actions
-      model.actionables = [];
       if(scope.showActions) {
-        jsonld.promises.frame(credential, actionFrame)
+        promise = jsonld.promises.frame(model.credential, actionFrame)
           .then(function(framed) {
-            model.actionables = framed['@graph'];
-          })
-          .catch(function(err) {
+            return framed['@graph'];
+          }).catch(function(err) {
             brAlertService.add('error', err, {scope: scope});
-          })
-          .then(function() {
-            scope.$apply();
           });
+      } else {
+        promise = Promise.resolve([]);
       }
-    }, true);
+
+      return promise.then(function(actionables) {
+        model.actionables = actionables;
+      });
+    }
   }
 }
 
