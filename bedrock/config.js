4b4ce06d16ad6f172d97ecb19047e7aed89cdca5
Dave Longley <dlongley@digitalbazaar.com>
2015-08-19 17:07:17 -0400

    Add default bedrock config.

---
+++ bedrock/config.js
@@ -0,0 +1,22 @@
+/*
+ * Bedrock Configuration.
+ *
+ * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(bedrock) {
+  var prepare = path.join(__dirname, 'prepare.js');
+  if(bedrock.config.protractor && fs.existsSync(prepare)) {
+    //var protractor = bedrock.config.protractor.config;
+    // add protractor tests
+    // protractor.suites['bedrock-angular-credential'] =
+    //   path.join(__dirname, './tests/**/*.js');
+    // protractor.params.config.onPrepare.push(prepare);
+  }
+
+  bedrock.config.views.vars['bedrock-angular-credential'] = {
+    libraries: {}
+  };
+};
