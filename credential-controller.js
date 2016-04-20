c324a301f91e1440b07e21fc2b61208c27e63584
Matthew Collier <collier.matthew@gmail.com>
2016-04-19 20:05:27 -0400

    Allow admin to view any credential.

--- credential-controller.js
+++ credential-controller.js
@@ -159,6 +159,9 @@ function factory(
         if(!('identity' in session) && self.credential.sysIsPublic) {
           // display a public credential to a non-authenticated user
           _display('credentialInfo');
+        } else if('CREDENTIAL_ADMIN' in session.identity.sysPermissionTable) {
+          // an admin is logged in, just show the credential
+          _display('credentialInfo');
         } else if(self.credential.claim.id === session.identity.id) {
           if(self.credential.sysState === 'unclaimed') {
             // the recipient is logged in, present acceptance directive
